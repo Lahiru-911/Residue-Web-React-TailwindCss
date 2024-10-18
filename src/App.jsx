0031d4a83547d3b9bc7c5a2adfb35438ac33579e
Lahiru Kavinda <lahiruxk@gmail.com>
2024-10-18 13:24:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ContactForm from "./Components/ContactForm";
 import ContactUs from "./Pages/ContactUs";
 import { WavyBackgroundDemo } from "./WavyBackgroundDemo";
 
@@ -5,9 +6,9 @@ import { WavyBackgroundDemo } from "./WavyBackgroundDemo";
 export default function App() {
   return (
     <div>
-      
+      <ContactForm/>
       <WavyBackgroundDemo/>
-      <ContactUs/>
+     <ContactUs/>
     
     
     </div>
